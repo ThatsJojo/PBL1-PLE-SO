52e0348a5ce6d5a3eece377c898ec7820ed68a56
Cleyton A Silva <cleyton.eu123@gmail.com>
2020-10-02 23:43:57 -0300

    Removendo alguns comentários

--- src/pbl/view/PBLPLESO.java
+++ src/pbl/view/PBLPLESO.java
@@ -17,41 +17,6 @@ public class PBLPLESO {
             System.out.println("Sistema Fechado");
             return;
         }
-//        String data = "Linha 1\nLinha 2\nLinha 3\nLinha 4\nLinha 5";
-//        String lido;
-//        
-//        try {
-//            lido = arquivoController.lerArquivo("Arquivo1.txt");
-//            System.out.println("A leitura inicial foi realizada.");
-//        } catch (NotTrackedFileException ex) {
-//            System.out.println("Arquivo não está listado");
-//            lido ="";
-//        } catch (IOException ex) {
-//            System.out.println("Arquivo1 estava vazio.");
-//            lido = "";
-//        }
-//        data = lido + data;
-//        try {
-//            arquivoController.escreverArquivo("Arquivo1.txt", data);
-//            System.out.println("Escrita realizada em Arquivo1");
-//        } catch (IOException ex) {
-//            System.out.println("Arquivo bugadoS");
-//        } catch (NotTrackedFileException ex) {
-//            System.out.println("Arquivo não trackeado 1");
-//        }
-//            
-//        
-//        
-//        try {
-//            lido = arquivoController.lerArquivo("Arquivo1.txt");
-//            System.out.println("Leitura no Arquivo1:\n"+lido);
-//        } catch (NotTrackedFileException ex) {
-//            System.out.println("Arquivo não trackeado 2");
-//        } catch (IOException ex) {
-//            System.out.println("Arquivo vazio");
-//        } 
-        
-        //Contador.getInstance().interrupt();
     }
     
 }
